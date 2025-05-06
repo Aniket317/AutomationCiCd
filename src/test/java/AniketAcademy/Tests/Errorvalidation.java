06c374dcff4fd0a47e5373bd98b9b0795ed689fe
Aniket <aniketpowar75@gmail.com>
2025-05-06 15:50:35 +0530

    Errorvalidation add

--- src/test/java/AniketAcademy/Tests/Errorvalidation.java
+++ src/test/java/AniketAcademy/Tests/Errorvalidation.java
@@ -13,7 +13,7 @@ public class Errorvalidation extends BaseTest{
 	@Test (groups = {"Error handling"}, retryAnalyzer =Retry.class)
 	public void submitOrder() throws IOException, InterruptedException
 	{		
-		
+		//testing ci cd
 		//div[@class='ng-tns-c4-11 ng-star-inserted ng-trigger ng-trigger-flyInOut ngx-toastr toast-error']
 		
 		landingPage.logindetails("aniketpowar22@gmail.co","Aniket@197731");		
